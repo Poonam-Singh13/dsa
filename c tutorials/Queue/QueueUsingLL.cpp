6525e9f0d183e95cc52795d9f1499482efdb9c57
Poonam-Singh13 <poonamsingh230994@acropolis.in>
2025-06-15 15:13:13 +0530

    Queue using linked list

---
+++ c tutorials/Queue/QueueUsingLL.cpp
@@ -0,0 +1,61 @@
+#include<iostream>
+using namespace std;
+
+class Node{
+    public:
+    int data;
+    Node* next;
+    Node(int data){
+        this->data=data;
+        this->next=NULL;
+    }
+};
+class Queue{
+    Node* head;
+    Node* tail;
+    public:
+    Queue(){
+        head=tail=NULL;
+    }
+    void push(int val){
+        Node*newNode=new Node(val);
+        if(head==NULL){
+            head=tail=newNode;
+        }else{
+            tail->next=newNode;
+            tail=newNode;
+        }
+    }
+    void pop(){
+         if(isEmpty()){
+            cout<<"queue is empty \n";
+            return ;
+        }
+        Node* temp=head;
+        head=head->next;
+        temp->next=NULL;
+        delete temp;
+    }
+    int front(){
+        if(isEmpty()){
+            cout<<"queue is empty \n";
+            return -1;
+        }
+        return head->data;
+    }
+    bool isEmpty(){
+        return head==NULL;
+    }
+};
+
+int main(){
+    Queue q;
+    q.push(1);
+    q.push(2);
+    q.push(3);
+    while(!q.isEmpty()){
+        cout<<q.front()<<"\n";
+        q.pop();
+    }
+    return 0;
+}
