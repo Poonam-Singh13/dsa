73689e090f8154b8cf5eabafaf73492172dd995a
Poonam-Singh13 <poonamsingh230994@acropolis.in>
2025-06-16 10:13:27 +0530

    Queue using deque

---
+++ c tutorials/Queue/queueUsingDeque.cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+#include<deque>
+using namespace std;
+class Queue{
+    deque<int>deq;
+    public:
+    void push(int data){
+        deq.push_back(data);
+    }
+    void pop(){
+        deq.pop_front();
+    }
+    int front(){
+        return deq.front();
+    }
+};
+int main(){
+    Queue q;
+    for(int i=1;i<=5;i++){
+        q.push(i);
+    }
+    for(int i=1;i<=5;i++){
+        cout<<q.front()<<endl;
+        q.pop();
+    }
+}
