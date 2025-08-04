fc17be95c040a9278209a9f6083056624d0c0409
Poonam-Singh13 <poonamsingh230994@acropolis.in>
2025-08-04 16:04:48 +0530

    Start with problem

---
+++ c tutorials/Tries/startWith.cpp
@@ -0,0 +1,60 @@
+#include<iostream>
+#include<unordered_map>
+#include<string>
+#include<vector>
+using namespace std;
+class Node{
+    public:
+    unordered_map<char,Node*>children;
+    bool endOfWord;
+    Node(){
+        endOfWord=false;
+    }
+};
+class Tries{
+    Node*root;
+    public:
+    Tries(){
+        root=new Node();
+    }
+    void insert(string key){
+        Node*temp=root;
+        for(int i=0;i<key.size();i++){
+            if(temp->children[key[i]]==NULL){
+                temp->children[key[i]]=new Node();
+            }
+            temp=temp->children[key[i]];
+        }
+        temp->endOfWord=true;
+    }
+    bool search(string key){
+        Node*temp=root;
+        for(int i=0;i<key.size();i++){
+            if(temp->children.count(key[i])){
+                temp=temp->children[key[i]];
+            }else{
+                return false;
+            }
+        }
+        return temp->endOfWord;
+    }
+    bool startWithPrefix(string prefix){
+        Node*temp=root;
+        for(int i=0;i<prefix.size();i++){
+            if(temp->children[prefix[i]]){
+                temp=temp->children[prefix[i]];
+            }else{
+                return false;
+            }
+        }
+        return true;
+    }
+};
+int main(){
+    vector<string>words={"the","there","their","a","any","thee"};
+    Tries tries;
+    for(int i=0;i<words.size();i++){
+        tries.insert(words[i]);
+    }
+    cout<<tries.startWithPrefix("p");
+}
